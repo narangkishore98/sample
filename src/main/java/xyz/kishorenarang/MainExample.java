6ede7ae7e7666caa25ee608ac7e35db92a296505
narangkishore98 <narangkishore98@gmail.com>
2019-05-24 19:09:49 -0400

    [Kishore] Array Reverse Funciton and Print Functions Added.

--- src/main/java/xyz/kishorenarang/MainExample.java
+++ src/main/java/xyz/kishorenarang/MainExample.java
@@ -5,7 +5,9 @@ public class MainExample
     public static void main(String[] args)
     {
         MainExample me = new MainExample();
-        int x[] = {1,2,3,4};
+        int x[] = {1,2,3,4,43,54,4,32};
+        x = me.reverseArray(x);
+        me.printArray(x);
 
         System.out.println("The Sum of Array is "+me.sumOfArray(x));
     }
@@ -17,5 +19,23 @@ public class MainExample
             sum+=arr[i];
         }
         return sum;
+    }
+    int[] reverseArray(int[] arr)
+    {
+        int rev[] = new int[arr.length];
+        int ni = 0;
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            rev[ni++] = arr[i];
+        }
+        return rev;
+    }
+    void printArray(int arr[]){
+        for(int i:arr)
+        {
+
+            System.out.print(i+"   ");
+
+        }
     }
 }
