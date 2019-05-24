cd49de3c5cc6e1d334c729f83253e9a5242aaf19
narangkishore98 <narangkishore98@gmail.com>
2019-05-24 18:56:32 -0400

    [Kishore] Array Sum

--- src/main/java/xyz/kishorenarang/MainExample.java
+++ src/main/java/xyz/kishorenarang/MainExample.java
@@ -2,7 +2,20 @@ package xyz.kishorenarang;
 
 public class MainExample
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        MainExample me = new MainExample();
+        int x[] = {1,2,3,4};
+
+        System.out.println("The Sum of Array is "+me.sumOfArray(x));
+    }
+    int sumOfArray(int[] arr)
+    {
+        int sum = 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+        }
+        return sum;
     }
 }
